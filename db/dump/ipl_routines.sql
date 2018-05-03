c1891125fa3a2cc2d0cca9c8be6997922722c05d
Vivek Dwivedi <thevivekdwivedi@gmail.com>
2018-05-03 22:39:19 +0530

    Updated with the latest information
    This dump has the latest data and the events, functions, procedures and triggers.

--- db/dump/ipl_routines.sql
+++ db/dump/ipl_routines.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `ipl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `ipl`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: ipl
@@ -18,6 +16,22 @@ USE `ipl`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Temporary view structure for view `spread`
+--
+
+DROP TABLE IF EXISTS `spread`;
+/*!50001 DROP VIEW IF EXISTS `spread`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `spread` AS SELECT 
+ 1 AS `matchID`,
+ 1 AS `Result1`,
+ 1 AS `Result2`,
+ 1 AS `Result3`,
+ 1 AS `Result4`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Temporary view structure for view `leaderboard`
 --
 
@@ -32,6 +46,45 @@ SET character_set_client = utf8;
  1 AS `net_profit_loss`,
  1 AS `efficiency`*/;
 SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `bidaudit`
+--
+
+DROP TABLE IF EXISTS `bidaudit`;
+/*!50001 DROP VIEW IF EXISTS `bidaudit`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `bidaudit` AS SELECT 
+ 1 AS `BidID`,
+ 1 AS `MatchID`,
+ 1 AS `Team1`,
+ 1 AS `Team2`,
+ 1 AS `FirstName`,
+ 1 AS `BidTeamNumber`,
+ 1 AS `BidValue`,
+ 1 AS `MatchResult`,
+ 1 AS `winningAmount`,
+ 1 AS `matchPool`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `spread`
+--
+
+/*!50001 DROP VIEW IF EXISTS `spread`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `spread` AS select `b1`.`matchID` AS `matchID`,(select sum(`b2`.`bidAmount`) from `bids` `b2` where ((`b2`.`matchID` = `b1`.`matchID`) and (`b2`.`predictedResult` = '1'))) AS `Result1`,(select sum(`b3`.`bidAmount`) from `bids` `b3` where ((`b3`.`matchID` = `b1`.`matchID`) and (`b3`.`predictedResult` = '2'))) AS `Result2`,(select sum(`b3`.`bidAmount`) from `bids` `b3` where ((`b3`.`matchID` = `b1`.`matchID`) and (`b3`.`predictedResult` = 'Tie'))) AS `Result3`,(select sum(`b4`.`bidAmount`) from `bids` `b4` where ((`b4`.`matchID` = `b1`.`matchID`) and (`b4`.`predictedResult` = 'Abandon'))) AS `Result4` from `bids` `b1` group by `b1`.`matchID` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
 -- Final view structure for view `leaderboard`
@@ -50,6 +103,32 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `bidaudit`
+--
+
+/*!50001 DROP VIEW IF EXISTS `bidaudit`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `bidaudit` AS select `b`.`bidID` AS `BidID`,`r`.`matchID` AS `MatchID`,`r`.`team1` AS `Team1`,`r`.`team2` AS `Team2`,`u`.`firstName` AS `FirstName`,`b`.`predictedResult` AS `BidTeamNumber`,`b`.`bidAmount` AS `BidValue`,`r`.`matchDecision` AS `MatchResult`,`b`.`winningAmount` AS `winningAmount`,`r`.`matchPool` AS `matchPool` from ((`bids` `b` join `roster` `r`) join `users` `u`) where ((`b`.`matchID` = `r`.`matchID`) and (`b`.`userID` = `u`.`userID`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Dumping events for database 'ipl'
+--
+
+--
+-- Dumping routines for database 'ipl'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -60,4 +139,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-01 22:37:46
+-- Dump completed on 2018-05-03 22:37:00
