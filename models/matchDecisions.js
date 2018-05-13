403a76858e14e652e12db21988d329bb73083fb3
Vivek Dwivedi <thevivekdwivedi@gmail.com>
2018-05-13 18:24:00 +0530

    Added match decisions model for DB API

---
+++ models/matchDecisions.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const db = require('../db/dbConnection');
+
+const bidStatuses = db.define('bidStatuses', {
+    decisionID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        validate: {
+            isNumeric: true
+        }
+    },
+    decisionName: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isAlphanumeric: true
+        }
+    }
+});
+
+module.exports = bidStatuses;
